23c915db24126db7f38e3ef74d7d9c0cb6fa9ee7
Yuta <y.sugiyama54@gmail.com>
2020-12-10 22:26:37 +0900

    インクリメントとデクリメント

---
+++ src/jp/hogehoge/javastudy/CountDown.java
@@ -0,0 +1,10 @@
+package jp.hogehoge.javastudy;
+
+public class CountDown {
+
+	public static void main(String[] args) {
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/jp/hogehoge/javastudy/CountUp.java
@@ -0,0 +1,10 @@
+package jp.hogehoge.javastudy;
+
+public class CountUp {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
