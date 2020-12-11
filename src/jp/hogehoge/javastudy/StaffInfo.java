5295c4e1fb6d63cc267fcf3072d51a6012b60328
Yuta <y.sugiyama54@gmail.com>
2020-12-11 17:04:09 +0900

    コンストラクタを使用時にフィールドに値を設定

--- src/jp/hogehoge/javastudy/StaffInfo.java
+++ src/jp/hogehoge/javastudy/StaffInfo.java
@@ -7,15 +7,23 @@ class Staff
 	String email;
 	
 	public void sayHello() {
-		System.out.println("Hello " + this.name);
+		System.out.println("名前 " + this.name);
+		System.out.println("社員番号 " + this.staffId);
+		System.out.println("メールアドレス " + this.email);
+	}
+
+	public Staff(String name, int staffId, String email) {
+		super();
+		this.name = name;
+		this.staffId = staffId;
+		this.email = email;
 	}
 }
 
 public class StaffInfo {
 
 	public static void main(String[] args) {
-		Staff yamada = new Staff();
-		yamada.name = "Taro Yamada";
+		Staff yamada = new Staff("Taro Yamada", 123456, "yamada_t@gmail.com");
 		
 		yamada.sayHello();
 	}
