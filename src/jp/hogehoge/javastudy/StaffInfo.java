8076558b1a3bf5573025b7db0571d4b84e143354
Yuta <y.sugiyama54@gmail.com>
2020-12-12 23:27:17 +0900

    ゲッターメソッドの定義

--- src/jp/hogehoge/javastudy/StaffInfo.java
+++ src/jp/hogehoge/javastudy/StaffInfo.java
@@ -18,14 +18,41 @@ class Staff
 		this.staffId = staffId;
 		this.email = email;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(int staffId) {
+		this.staffId = staffId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
 
 public class StaffInfo {
 
 	public static void main(String[] args) {
 		Staff yamada = new Staff("Taro Yamada", 123456, "yamada_t@gmail.com");
-		
-		yamada.sayHello();
+		// yamada.sayHello();
+		System.out.println("社員情報");
+		System.out.println("氏名　" + yamada.getName());
+		System.out.println("社員番号　" + yamada.getStaffId());
+		System.out.println("メールアドレス　" + yamada.getEmail());
 	}
 
 }
